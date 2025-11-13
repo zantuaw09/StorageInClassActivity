440c5334f8aa4363ce0fe051afdb2806159a285a
Wyatt Zantua <tuo18351@temple.edu>
2025-11-13 14:35:26 -0500

    Implements auto load of previously saved comic on app start or rotation

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -6,14 +6,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save comic info when downloaded
@@ -28,9 +30,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
 
+    private val savedComicFile = "savedComicFile"
+    private lateinit var file: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // creates file for saved comic JSON
+        file = File(filesDir, savedComicFile)
 
         requestQueue = Volley.newRequestQueue(this)
 
@@ -39,6 +47,24 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
+
+        // load comic info if a previous comic was saved
+        if (file.exists()) {
+            try {
+                val br = BufferedReader(FileReader(file))
+                val jsonText = StringBuilder()
+                var line: String?
+
+                while (br.readLine().also { line = it } != null) {
+                    jsonText.append(line)
+                }
+
+                showComic(JSONObject(jsonText.toString()))
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
@@ -51,7 +77,10 @@ class MainActivity : AppCompatActivity() {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
             JsonObjectRequest(url
-                , {showComic(it)}
+                , {
+                    showComic(it)
+                    saveComic(it)
+                  }
                 , {}
             )
         )
@@ -64,9 +93,15 @@ class MainActivity : AppCompatActivity() {
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
     }
 
-    // Implement this function
+    // Saved comic JSON as string file
     private fun saveComic(comicObject: JSONObject) {
-
+        try {
+            val outputStream = FileOutputStream(file)
+            outputStream.write(comicObject.toString().toByteArray())
+            outputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 
